973632001033d8d4f6a83b8055401e2ce5df1f76
Aniket Shah <aniketshah422@gmail.com>
2022-11-08 15:04:16 +0530

    group chat updated

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -7,7 +7,7 @@ const Chat = require("../models/chatModel")
 const User = require('../models/user')
 const { populate } = require('../models/user')
 const Message = require("../models/messageModel")
-const { latestMessage, groupAdmin } = require("../models/chatModel")
+const { latestMessage, groupAdmin, isGroupChat } = require("../models/chatModel")
 
 //for accessing chat (accessChat)
 router.post('/chat', requireLogin, async (req, res) => {
@@ -17,7 +17,7 @@ router.post('/chat', requireLogin, async (req, res) => {
         return res.sendStatus(400)
     }
     var isChat = await Chat.find({
-        isGoupChat: false,
+        isGroupChat: false,
         $ans: [
             {users: {$elementMatch:{$eq:req.user._id }}},
             {users: {$elementMatch:{$eq: userId }}}
@@ -87,12 +87,12 @@ router.post('/chat/group', requireLogin, async (req, res)=> {
         .send("More than 2 users are required to form a group chat");
     }
 
-    users.push(req.user);
+    users.push(req.user)
     try {
         const groupChat = await Chat.create({
             chatName: req.body.name,
             users: users,
-            isGoupChat: true,
+            isGroupChat: true,
             groupAdmin: req.user,
         });
 
@@ -108,18 +108,59 @@ router.post('/chat/group', requireLogin, async (req, res)=> {
 
 
 //rename reanameGroup
-router.put('chat/rename', requireLogin, (req, res)=> {
-
-})
-
-//groupremove removeFromGroup
-router.put('/chat/groupremove', requireLogin, (req, res) => {
-
+router.put('/chat/rename', requireLogin, async (req, res)=> {
+    const { chatId, chatName } = req.body;
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName,
+        },
+        {
+            new: true,
+        }
+    ).populate("users", "-password")
+    .populate("groupAdmin", "-password");
+    if(!updatedChat) {
+        res.status(404);
+        throw new Error("Chat Not found");
+    } else {
+        res.json(updatedChat);
+    }
 })
 
 //groupadd addtoGroup
-router.put('/chat/groupadd', requireLogin, (req, res) => {
-
+router.put('/chat/groupadd', requireLogin, async (req, res) => {
+    const { chatId, userId } = req.body;
+    const added = await Chat.findByIdAndUpdate(chatId, {
+        $push: { users: userId },  
+    }, 
+    { new: true }
+    ).populate("users", "-password")
+    .populate("groupAdmin", "-password");
+    if(!added) {
+        res.status(404);
+        throw new Error("Chat not found");
+    } else {
+        res.json(added);
+    }
 })
+
+//groupremove removeFromGroup
+router.put('/chat/groupremove', requireLogin, async (req, res) => {
+    const { chatId, userId } = req.body;
+    const removed = await Chat.findByIdAndUpdate(chatId, {
+        $pull: { users: userId },  
+    }, 
+    { new: true }
+    ).populate("users", "-password")
+    .populate("groupAdmin", "-password");
+    if(!removed) {
+        res.status(404);
+        throw new Error("Chat not found");
+    } else {
+        res.json(removed);
+    }
+})
+
 
 module.exports = router
